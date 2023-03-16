c998d3994bea732f9e3cf2c31e1f7b2536514a66
YardleighAnthony <yardleighanthony@gmail.com>
2023-03-16 07:34:29 +0200

    UPDATES

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-let menu = document.querySelector('#menu-icon');
+
+
+/*let menu = document.querySelector('#menu-icon');
 let naviBar = document.querySelector('.naviBar');
 
 menu.onclick = () =>{
     menu.classList.toggle('bx-x');
     naviBar.classList.toggle('open');
 
-}
+}*/
+
+//Sticky nav bar
+window.onscroll = () =>{
+let header = document.querySelector('.header')
+header.classList.toggle('sticky',window.scrollY > 100)};
