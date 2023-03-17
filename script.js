0fab9b3398c74444f8fba01f54ca13195b6c02b4
YardleighAnthony <yardleighanthony@gmail.com>
2023-03-17 16:23:07 +0200

    Update

--- script.js
+++ script.js
@@ -10,6 +10,27 @@ menu.onclick = () =>{
 }*/
 
 //Sticky nav bar
+/*Scoll section action*/
+let sections = document.querySelectorAll('section')  ; 
+let nav = document.querySelectorAll('header naviBar a')  ; 
+//Sticky nav bar
 window.onscroll = () =>{
+sections.forEach(sec =>{
+
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top>= offset && top < offset + height){
+        navLinks.forEach(links =>{
+            links.classList.remove('active');
+            document.querySelector('header naviBar a[href*=' + id + ']').classList.add('active');
+        });
+    }
+});
+
 let header = document.querySelector('.header')
-header.classList.toggle('sticky',window.scrollY > 100)};
+header.classList.toggle('sticky',window.scrollY > 100)
+
+};
